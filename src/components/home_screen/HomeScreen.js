2b8add1fa1bb88be42d81c93b47c02358fa1eed8
Kathy Martinez <katherynymartinez@gmail.com>
2020-04-02 19:39:51 -0400

    edited home screen instructions and header

--- src/components/home_screen/HomeScreen.js
+++ src/components/home_screen/HomeScreen.js
@@ -9,15 +9,27 @@ class HomeScreen extends Component {
   handleFreestyleGame = () => {
     this.props.startFreestyleCallback();
   }
+
+  handleGoHome = () => {
+    this.props.goToHomeCallback();
+}
   
   render() {
     return (
       <div className="App" style={{textAlign: "center", padding:10, margin: 10}}>
-        <Header/>
-        <p style={{fontSize: 20, padding: 30}}>
-          Defeat the monster by typing out as many<br/>words as you can before the time runs out.
-        </p>
-        <button className="button" onClick={this.handleMatchGame} style={{fontSize: 30, cursor: "pointer", margin: 10}}>MATCH</button>
+        <Header goToHomeCallback={this.handleGoHome}/>
+        <div style={{padding: 10}}>
+          <span>Match mode:
+            Defeat the monster by typing out as many<br/>words as you can before the time runs out.</span>
+            <br/><br/>
+          <span>Freestyle mode:
+            Type as many words as you can in a minute.
+          </span>
+        </div>
+        <div style={{padding: 10}}>
+          <button className="button" onClick={this.handleMatchGame} style={{fontSize: 30, cursor: "pointer", margin: 10}}>MATCH</button>
+          <button className="button" onClick={this.handleFreestyleGame} style={{fontSize: 30, cursor: "pointer", margin: 10}}>FREESTYLE</button>
+        </div> 
       </div>
     );
   }
