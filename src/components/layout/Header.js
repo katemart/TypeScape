bc1ad2569c6faa08d7ea6734c8cc8c1e7669ddc3
Kathy Martinez <katherynymartinez@gmail.com>
2020-04-02 19:38:23 -0400

    modified header to be clickable, modified monster img

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,10 +2,20 @@ import React, {Component} from 'react';
 import title from './images/title.png'
 
 class Header extends Component {
+  handleGoHome = () => {
+    this.props.goToHomeCallback();
+  }
+
       render() {
         return (
             <header>
-                <img src={title} width={400} className="App-title" alt="title"/>
+                <img 
+                  src={title} 
+                  width={400} 
+                  className="App-title" 
+                  alt="title" 
+                  style={{cursor: "pointer"}}
+                  onClick={this.handleGoHome}/>
             </header>
         )
       }
