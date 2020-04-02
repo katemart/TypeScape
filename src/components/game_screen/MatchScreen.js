ab5be452be463c6abe2266451983e988f9ed3a83
Kathy Martinez <katherynymartinez@gmail.com>
2020-04-02 19:39:01 -0400

    changed functionality to use input box

--- src/components/game_screen/MatchScreen.js
+++ src/components/game_screen/MatchScreen.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Header from '../layout/Header';
 import {words} from '../../utils/words.js';
-import monster from '../layout/images/goop.gif';
+import monster from '../layout/images/bubbles.gif';
 import monster_final from '../layout/images/goop_end.png';
 import HealthBar from '../layout/HealthBar';
 
@@ -16,23 +16,22 @@ class MatchScreen extends Component {
         super(props);
         this.state = {
             activeWord: this.getWord(),
-            activeLetters: [],
             score: 0,
             health: 100,
-            time: 10,
+            time: 60,
             isPlaying: true, 
             status: Status.DEFAULT
         }
-      }
+    }
 
     componentDidMount = () => {
-        this.intervalId = setInterval(this.countdown.bind(this), 1000);
-        document.addEventListener("keydown", this.handleKeyPress);
-        setInterval(this.checkStatus.bind(this), 50);
+        this.countInterval = setInterval(this.countdown.bind(this), 1000);
+        this.statusInterval = setInterval(this.checkStatus.bind(this), 50);
     }
 
     componentWillUnmount = () => {
-        clearInterval(this.intervalId);
+        clearInterval(this.countInterval);
+        clearInterval(this.statusInterval);
     }
 
     handleGoHome = () => {
@@ -50,77 +49,83 @@ class MatchScreen extends Component {
             time: this.state.time - 1
         })
         if(this.state.time < 1) {
-            clearInterval(this.intervalId);
+            clearInterval(this.countInterval);
         }
     }
 
     checkStatus = () => {
-        if(this.state.time >= 0 && this.state.health === 0) {
-            document.removeEventListener("keydown", this.handleKeyPress);
+        if(this.state.time > 0 && this.state.health === 0) {
             this.setState({
                 status: Status.WINNER,
                 isPlaying: false
             })
         } else if(this.state.time <= 0 && this.state.health > 0){
-            document.removeEventListener("keydown", this.handleKeyPress)
             this.setState({
                 status: Status.LOSER,
                 isPlaying: false
             })
-        }
-    }
-
-    handleKeyPress = (e) => {
-        e.preventDefault();
-        // handle backspace and delete (both work the same way on mac)
-        if(e.which === 46 || e.which === 8){
-            this.setState({
-                activeLetters: this.state.activeLetters.slice(0,-1)
-            })
         } 
-        // handle other keys
-        if(e.which >= 65 && e.which <= 90) {
-            let char = String.fromCharCode(e.which);
-            let word = this.state.activeWord;
-            let typed = this.state.activeLetters;
-            typed.push(char);
-            if(this.checkStrings(typed, word)) {
-                this.setState({
-                    activeWord: this.getWord(),
-                    activeLetters: [],
-                    score: this.state.score + 10,
-                    health: this.state.health - 50
-                })
-            } else {
-                this.setState({
-                    activeLetters: typed
-                })
-            }
+    }
+
+    calculateHealth = () => {
+        let health = this.state.health;
+        if(this.state.activeWord.length <= 5 && health > 5) {
+            return health - 5;
+        } else if((this.state.activeWord.length > 5 && this.state.activeWord.length <= 10) && health > 10) {
+            return health - 10;
+        } else if(this.state.activeWord.length > 10 && health > 20) {
+            return health - 20;
+        } else {
+            health = 0;
+            return health;
         }
     }
 
-    checkStrings(s1, s2) {
-        if(s1.length !== s2.length) 
-            return false;
-        for(let i = s1.length; i--;) {
-            if(s1[i] !== s2[i]) 
-                return false;
+    calculateScore = () => {
+        let score = this.state.score;
+        if(this.state.activeWord.length <= 5) {
+            return score + 5;
+        } else if(this.state.activeWord.length <= 8) {
+            return score + 10;
+        } else {
+            return score + 15;
         }
-        return true;
+    }
+
+    checkWord = (e) => {
+        if(e.target.value.toUpperCase() === this.state.activeWord) {
+            this.setState({
+                activeWord: this.getWord(),
+                score: this.calculateScore(),
+                health: this.calculateHealth()
+            })
+            e.target.value = "";
+        } 
     }
 
     render() {
         switch(this.state.isPlaying) {
             case true:
                 return (
-                    <div className="App">
-                        <div style={{textAlign: "center", padding:10, margin: 10}}>
-                            <Header/>
-                            <h2>{this.state.activeWord}</h2>
-                            <img src={monster} width={250} className="App-title" alt="title"/>
+                    <div>
+                        <div style={{textAlign: "center", padding: 10, margin: 10}}>
+                            <Header goToHomeCallback={this.handleGoHome}/>
+                            <div>
+                                <h2>{"TIME: " + this.state.time + "\tSCORE: " + this.state.score}</h2>
+                            </div>
+                            <br/>
+                            <img src={monster} width={250} alt="title"/>
                             <HealthBar width={this.state.health}/>
-                            <div className="col s4">
-                                <h4>{"TIME: " + this.state.time + "\tSCORE: " + this.state.score}</h4>
+                            <h1 style={{fontSize: 50}}>{this.state.activeWord}</h1>
+                            <div style={{padding: 20}}>
+                                <input 
+                                    className="word-input"
+                                    type="text" 
+                                    placeholder="Start typing..." 
+                                    style={{textTransform: "uppercase"}} 
+                                    onInput={this.checkWord}
+                                    autoFocus
+                                />
                             </div>
                         </div>
                     </div>
@@ -129,17 +134,17 @@ class MatchScreen extends Component {
                 return (
                     <div>
                         <div style={{textAlign: "center", padding: 5, margin: 5}}>
-                            <Header/>
-                            <h2>{this.state.activeWord}</h2>
-                            <img src={monster_final} width={250} className="App-title" alt="title"/>
-                            <div className="col s4">
-                                <h4>GAME OVER!</h4>
-                                <h5>YOU HAVE {this.state.status} THE GAME.</h5>
-                                <h6>FINAL SCORE: {this.state.score}</h6>
+                            <Header goToHomeCallback={this.handleGoHome}/>
+                            <img src={monster_final} width={250} alt="title"/>
+                            <div>
+                                <h1>GAME OVER!</h1>
+                                <h2>YOU HAVE {this.state.status} THE GAME.</h2>
+                                <h3>FINAL SCORE: {this.state.score}</h3>
                             </div>
                         </div>
                         <div style={{textAlign: "center", padding:10, margin: 10}}>
-                            <button className="button" onClick={this.handleGoHome} style={{fontSize: 30, cursor: "pointer", margin: 10}}>PLAY AGAIN</button>
+                            <button className="button" onClick={this.handleGoHome} 
+                            style={{fontSize: 30, cursor: "pointer", margin: 10}}>PLAY AGAIN</button>
                         </div>
                     </div>
                 );
