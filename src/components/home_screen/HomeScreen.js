e4f84cca3906526e93dc5bb231eab29b5486f70e
Kathy Martinez <katherynymartinez@gmail.com>
2020-04-06 12:15:37 -0400

    fixed home screen styling

--- src/components/home_screen/HomeScreen.js
+++ src/components/home_screen/HomeScreen.js
@@ -21,43 +21,51 @@ difficultyClick = (e) => {
   
   render() {
     return (
-      <div className="App" style={{padding:10, margin: 10}}>
+      <div className="App" style={{padding: 10, margin: 15}}>
         <div style={{textAlign: "center"}}>
-        <Header goToHomeCallback={this.handleGoHome}/>
+          <Header goToHomeCallback={this.handleGoHome}/>
         </div>
-        
-        <div style={{padding: 10}}>
-          <span>
-            PLAY MODES:
+        <div style={{margin: "auto", width: "80%"}}>
+          <div>
+            <span>
+              PLAY MODES:
+              <br/>
+              Match mode:
+              Defeat the monster by typing out as many words as you can before the time runs out.
+              <br/>
+              Freestyle mode:
+              Type as many words as you can before time runs out.
+            </span>
+          </div>
+          <br/>
+          <form>
+            <span>
+              SELECT A DIFFICULTY LEVEL: 
+              <br/>
+              If no level is chosen, the level will be set to "EASY" by default.
+            </span>
             <br/>
-            Match mode:
-            Defeat the monster by typing out as many words as you can before the time runs out.
-            <br/>
-            Freestyle mode:
-            Type as many words as you can in a minute.
-          </span>
+            <label>
+                <input name="level" type="radio" value={this.props.Levels.EASY} onClick={this.difficultyClick}></input>
+                &nbsp;EASY (120 seconds)
+              </label>
+            <label style={{padding:10}}>
+                <input name="level" type="radio" value={this.props.Levels.MEDIUM} onClick={this.difficultyClick}></input>
+                &nbsp;MEDIUM (60 seconds)
+            </label>
+            <label style={{padding:10}}>
+                <input name="level" type="radio" value={this.props.Levels.HARD} onClick={this.difficultyClick}></input>
+                &nbsp;HARD (30 seconds)
+            </label>
+          </form>
+          <br/>
+          <div>
+            <span>SELECT A PLAY MODE TO START GAME:</span>
+            <br></br>
+            <button className="button" onClick={this.handleMatchGame} style={{fontSize: 30, cursor: "pointer"}}>MATCH</button>
+            <button className="button" onClick={this.handleFreestyleGame} style={{fontSize: 30, cursor: "pointer", margin: 10}}>FREESTYLE</button>
+          </div> 
         </div>
-        <span style={{padding: 10}}>SELECT A DIFFICULTY LEVEL: </span>
-        <form>
-            <label style={{padding:10}}>
-              <input name="level" type="radio" value={this.props.Levels.EASY} onClick={this.difficultyClick}></input>
-              &nbsp;EASY (120 seconds)
-            </label>
-            <label style={{padding:10}}>
-              <input name="level" type="radio" value={this.props.Levels.MEDIUM} onClick={this.difficultyClick}></input>
-              &nbsp;MEDIUM (60 seconds)
-            </label>
-            <label style={{padding:10}}>
-              <input name="level" type="radio" value={this.props.Levels.HARD} onClick={this.difficultyClick}></input>
-              &nbsp;HARD (30 seconds)
-            </label>
-        </form>
-        <div style={{padding: 10}}>
-          <span>CLICK A GAME MODE TO START GAME:</span>
-          <br></br>
-          <button className="button" onClick={this.handleMatchGame} style={{fontSize: 30, cursor: "pointer", margin: 10}}>MATCH</button>
-          <button className="button" onClick={this.handleFreestyleGame} style={{fontSize: 30, cursor: "pointer", margin: 10}}>FREESTYLE</button>
-        </div> 
       </div>
     );
   }
